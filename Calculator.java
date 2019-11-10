a92873dd6d5997304ce40966c1a2f83f626f0a31
JHJ <jung0984@gmail.com>
2019-11-10 15:57:36 +0900

    Add practice

--- Calculator.java
+++ Calculator.java
@@ -9,4 +9,8 @@ public class Calculator{
   public static int multiplication(int a, int b) {
     return a * b; // comment
   }
+
+  public static int multiplication2(int a, int b) {
+    return a * b; // Plz.. Use my code
+  }
 }
